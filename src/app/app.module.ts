335c8df731e4f48d7ad4935e22bd251bf0267d3d
mpicat <maxime.picat@coursdiderot.com>
2022-07-05 13:51:21 +0200

    MAJ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    AccueilComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
