baedacd905430edb28d887f6f5d34b5849cd5e96
mpicat <maxime.picat@coursdiderot.com>
2022-07-10 15:58:31 +0200

    MAJ

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AProposComponent } from './a-propos/a-propos.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { ConfidentialiteComponent } from './confidentialite/confidentialite.component';
 
 const routes: Routes = [
   { path: '', component: AccueilComponent},
-  { path: 'apropos', component: AProposComponent}
+  { path: 'apropos', component: AProposComponent},
+  { path: 'confidentialite', component: ConfidentialiteComponent}
 ];
 
 @NgModule({
